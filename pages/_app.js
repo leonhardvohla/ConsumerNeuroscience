602093d42df8904f6bb394a1f05ee2afdada46fa
louiscutner <56656319+louiscutner@users.noreply.github.com>
2024-07-27 17:09:49 +0900

    added favicon and site name

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import "../styles.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Consumer Neuroscience</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
